92df4ac62906463d969756480483f44804f4ebc6
root <root@ip-172-26-6-231.ap-southeast-1.compute.internal>
2019-08-19 17:52:37 +0800

    adjustment

--- errorlog.py
+++ errorlog.py
@@ -1,6 +1,6 @@
 import re
 import json
-f = open("error.log","r")
+f = open("/var/log/apache2/error.log","r")
 content = f.readlines()
 
 def log_extract(string_lines):
@@ -8,20 +8,17 @@ def log_extract(string_lines):
     output = {}
     if string_array[0] != '\n':
         trip = string_array[0].strip()
-        info = string_array[1].strip().split(' in ')
-        cause = info[0].strip()
-        file_refer = info[1].split(',')
-        filename = file_refer[0]
-        referrer = file_refer[1]
+        info = string_array[1].strip()
+        cause = info.strip().split(",")
         clean_info = re.findall("\[(.*?)\]", trip) 
         output = {
             "info": {
                 "time": clean_info[0],
                 "client": clean_info[-1].replace("client ","").split(':')[0],
-                "referrer": referrer
+                "referrer": cause[1]
             },
-            "cause": cause,
-            "filename": filename
+            "cause": cause[0],
+            "filename": cause[1]
         }
     return output
 
@@ -31,21 +28,22 @@ files = {}
 
 for x in content:
     data = log_extract(x)
-    if data['info']['client'] in clients:
-        clients[data['info']['client']] += 1
-    else:
-        clients[data['info']['client']] = 1
+    if 'info' in data:
+        if data['info']['client'] in clients:
+       	 clients[data['info']['client']] += 1
+        else:
+         clients[data['info']['client']] = 1
     
-    data_cause = data['cause'] + ' @ ' + data['filename']
-    if data_cause in clients:
-        causes[data_cause] += 1
-    else:
-        causes[data_cause] = 1
+        data_cause = data['cause']
+        if data_cause in clients:
+         causes[data_cause] += 1
+        else:
+         causes[data_cause] = 1
 
-    if data['filename'] in clients:
-        files[data['filename']] += 1
-    else:
-        files[data['filename']] = 1
+        if data['filename'] in clients:
+         files[data['filename']] += 1
+        else:
+         files[data['filename']] = 1
 
 reports = {
     "client": clients,
