0346b99398ea6f949d68ea05ce775d54c349d002
onewoorks <irwanbibrahim@onewoorks.com>
2019-08-19 17:10:43 +0800

    basic log viewer

---
+++ errorlog.py
@@ -0,0 +1,54 @@
+import re
+import json
+f = open("error.log","r")
+content = f.readlines()
+
+def log_extract(string_lines):
+    string_array = string_lines.split(" PHP Notice:")
+    trip = string_array[0].strip()
+    info = string_array[1].strip().split(' in ')
+    cause = info[0].strip()
+    file_refer = info[1].split(',')
+    filename = file_refer[0]
+    referrer = file_refer[1]
+    clean_info = re.findall("\[(.*?)\]", trip) 
+    output = {
+        "info": {
+            "time": clean_info[0],
+            "client": clean_info[-1].replace("client ","").split(':')[0],
+            "referrer": referrer
+        },
+        "cause": cause,
+        "filename": filename
+    }
+    return output
+
+clients = {}
+causes = {}
+files = {}
+
+for x in content:
+    data = log_extract(x)
+    if data['info']['client'] in clients:
+        clients[data['info']['client']] += 1
+    else:
+        clients[data['info']['client']] = 1
+    
+    data_cause = data['cause'] + ' @ ' + data['filename']
+    if data_cause in clients:
+        causes[data_cause] += 1
+    else:
+        causes[data_cause] = 1
+
+    if data['filename'] in clients:
+        files[data['filename']] += 1
+    else:
+        files[data['filename']] = 1
+
+reports = {
+    "client": clients,
+    "causes": causes
+}
+print(json.dumps(reports))
+
+f.close()
